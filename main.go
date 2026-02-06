0b431e199c92926540003bd403b1325f7480c76d
Alan Aether <AlanAetherX@gmail.com>
2026-02-06 15:08:40 +0530

    Fix DNS challenge flow structure

--- main.go
+++ main.go
@@ -230,7 +230,7 @@ func completeIssuance() {
 func basicAuth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		user, pass, ok := r.BasicAuth()
-		if !ok || user != "admin" || pass != "yourpassword" {
+		if !ok || user != "admin" || pass != "your73password" {
 			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
